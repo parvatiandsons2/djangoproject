219e19a813f6e7c71ce39ba8bd0a9d12dc51e86e
ParvatiAndSons <parvatiandsons2@gmail.com>
2020-04-14 08:31:11 +0530

    update storage bucket

--- CompanyProject/settings.py
+++ CompanyProject/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+from google.oauth2 import service_account
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+Is_Live = 0
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -24,10 +26,13 @@ SECRET_KEY = '7(@iyr&+5enzbmwyg+o6$&vhy@enhgp*w8n+bis1gt&l4^6=g#'
 # SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
 
-ALLOWED_HOSTS = ['35.200.244.164','127.0.0.1']
+if Is_Live == 1:
+    DEBUG = False
+else:
+    DEBUG = True
+
+ALLOWED_HOSTS = ['35.200.244.164', '127.0.0.1']
 
 
 # Application definition
@@ -83,43 +88,37 @@ WSGI_APPLICATION = 'CompanyProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+if Is_Live == 1:
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'companyproject',
-    #     'USER': 'root',
-    #     'PASSWORD': '@Parvati!123',
-    #     # 'HOST': '127.0.0.1',
-    #     'HOST': 'localhost',
-    #     'PORT':'3306'
-    # }
-
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'companyproject',
-        'USER': 'companyproject',
-        'PASSWORD': 'Oyfsbho4sBGFngn9',
-        # 'HOST': '127.0.0.1',
-        'HOST': '/cloudsql/staticwebsite-274104:asia-northeast2:companyproject',
-        'HOST': '34.97.28.181',
-        'PORT': '3306'
+    DATABASES = {
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # }
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'companyproject',
+            'USER': 'companyproject',
+            'PASSWORD': 'Oyfsbho4sBGFngn9',
+            # 'HOST': '127.0.0.1',
+            'HOST': '/cloudsql/staticwebsite-274104:asia-northeast2:companyproject',
+            'HOST': '34.97.28.181',
+            'PORT': '3306'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'companyproject',
+            'USER': 'root',
+            'PASSWORD': '@Parvati!123',
+            # 'HOST': '127.0.0.1',
+            'HOST': 'localhost',
+            'PORT': '3306'
+        }
+    }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#             'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf'),
-#         },
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -154,22 +153,49 @@ USE_L10N = True
 USE_TZ = True
 
 
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(
+        BASE_DIR, 'staticwebsite-274104-a13c760a65fd.json')
+)
+
+# Google Cloud Storage
+if Is_Live == 1:
+    GOOGLE_APPLICATION_CREDENTIALS = os.path.join(
+        BASE_DIR, 'staticwebsite-274104-a13c760a65fd.json')
+    DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+    GS_ACCESS_KEY_ID = 'GOOG1EVPUZAMBWWHYZVOSVOQT5JAIFXFHRBK652MGE4TZKMMXIW7Y3LWUW5EQ'
+    GS_SECRET_ACCESS_KEY = 'wiOwL/zcWpHAZ9HKSuYl5wvUdEIn+AXc3tVJWbe2'
+    GS_BUCKET_NAME = 'prnson_bucket1/static'
+
+    STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+else:
+    GOOGLE_APPLICATION_CREDENTIALS = os.path.join(
+        BASE_DIR, 'staticwebsite-274104-a13c760a65fd.json')
+    DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+    GS_ACCESS_KEY_ID = 'GOOG1EVPUZAMBWWHYZVOSVOQT5JAIFXFHRBK652MGE4TZKMMXIW7Y3LWUW5EQ'
+    GS_SECRET_ACCESS_KEY = 'wiOwL/zcWpHAZ9HKSuYl5wvUdEIn+AXc3tVJWbe2'
+    GS_BUCKET_NAME = 'prnson_bucket1/static'
+
+    STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
+if Is_Live == 1:
+    STATICFILES_DIRS = [
+        'https://storage.googleapis.com/prnson_bucket1/static/', ]
+    STATIC_URL = 'https://storage.googleapis.com/prnson_bucket1/static/'
+else:
+    STATICFILES_DIRS = [BASE_DIR+'\\static']
+    STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ['https://storage.googleapis.com/prnson_bucket1/static/',]
-STATIC_URL = 'https://storage.googleapis.com/prnson_bucket1/static/'
-
-# STATICFILES_DIRS = [BASE_DIR+'\\static']
-# STATIC_URL = '/static/'
-
-
-MEDIA_ROOT = 'https://storage.googleapis.com/prnson_bucket1/media/'
-MEDIA_URL = 'https://storage.googleapis.com/prnson_bucket1/media/'
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-# MEDIA_URL = '/media/'
+if Is_Live == 1:
+    MEDIA_ROOT = 'https://storage.googleapis.com/prnson_bucket1/media/'
+    MEDIA_URL = 'https://storage.googleapis.com/prnson_bucket1/media/'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+    MEDIA_URL = '/media/'
