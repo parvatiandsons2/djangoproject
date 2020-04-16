901062c839dde608d9c2c865b61dbc0902988e66
ParvatiAndSons <parvatiandsons2@gmail.com>
2020-04-16 17:56:29 +0530

    alter website

--- website/migrations/0049_auto_20200416_1707.py
+++ website/migrations/0049_auto_20200416_1707.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='companystats',
             name='created_by',
-            field=models.ForeignKey(default=0, editable=False, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(editable=False, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
