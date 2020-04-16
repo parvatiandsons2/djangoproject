222b71382cf2c8c62654a6482e7a45e8126cfe56
ParvatiAndSons <parvatiandsons2@gmail.com>
2020-04-16 17:25:09 +0530

    alter project

--- CompanyProject/settings.py
+++ CompanyProject/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-Is_Live = 1
+Is_Live = 0
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -28,7 +28,7 @@ SECRET_KEY = '7(@iyr&+5enzbmwyg+o6$&vhy@enhgp*w8n+bis1gt&l4^6=g#'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 if Is_Live == 1:
-    DEBUG = False
+    DEBUG = True
 else:
     DEBUG = True
 
